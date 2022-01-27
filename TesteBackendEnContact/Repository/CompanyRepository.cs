d6094a5c0016f76069c8dcac83ad2b8ff11b4877
Ronaldo Romério <ronaldo.romeriosj@gmail.com>
2022-01-26 21:05:30 -0300

    Correção dos métodos company

--- TesteBackendEnContact/Repository/CompanyRepository.cs
+++ TesteBackendEnContact/Repository/CompanyRepository.cs
@@ -26,10 +26,7 @@ namespace TesteBackendEnContact.Repository
             using var connection = new SqliteConnection(databaseConfig.ConnectionString);
             var dao = new CompanyDao(company);
 
-            if (dao.Id == 0)
-                dao.Id = await connection.InsertAsync(dao);
-            else
-                await connection.UpdateAsync(dao);
+            dao.Id = await connection.InsertAsync(dao);
 
             return dao.Export();
         }
@@ -37,13 +34,17 @@ namespace TesteBackendEnContact.Repository
         public async Task DeleteAsync(int id)
         {
             using var connection = new SqliteConnection(databaseConfig.ConnectionString);
+            connection.Open();
             using var transaction = connection.BeginTransaction();
 
             var sql = new StringBuilder();
             sql.AppendLine("DELETE FROM Company WHERE Id = @id;");
             sql.AppendLine("UPDATE Contact SET CompanyId = null WHERE CompanyId = @id;");
 
-            await connection.ExecuteAsync(sql.ToString(), new { id }, transaction);
+            await connection.ExecuteAsync(sql.ToString(), new {id}, transaction);
+
+            transaction.Commit();
+            connection.Close();
         }
 
         public async Task<IEnumerable<ICompany>> GetAllAsync()
@@ -53,17 +54,17 @@ namespace TesteBackendEnContact.Repository
             var query = "SELECT * FROM Company";
             var result = await connection.QueryAsync<CompanyDao>(query);
 
-            return result?.Select(item => item.Export());
+            return result.ToList();
         }
 
         public async Task<ICompany> GetAsync(int id)
         {
             using var connection = new SqliteConnection(databaseConfig.ConnectionString);
 
-            var query = "SELECT * FROM Conpany where Id = @id";
-            var result = await connection.QuerySingleOrDefaultAsync<CompanyDao>(query, new { id });
+            var query = $"SELECT * FROM Company where Id = @id";
+            var result = await connection.QuerySingleOrDefaultAsync<CompanyDao>(query, new {id});
 
-            return result?.Export();
+            return result;
         }
     }
 
